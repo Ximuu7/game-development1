6ef93608090ee3aa61aad74ac40fbcfad19d2bd1
Ximuu7 <783718396@qq.com>
2026-05-06 08:29:39 +0800

    第一次提交

---
+++ Assets/FitScreenSize_Sprite.cs
@@ -0,0 +1,33 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FitScreenSize_Sprite : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+
+
+    public void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        FitScreenSize();
+    }
+
+    public void FitScreenSize()
+    {
+        Camera camera = Camera.main;
+        float screenY = camera.orthographicSize * 2f;
+        float screenX = screenY * camera.aspect;
+
+        float spriteY=spriteRenderer.sprite.bounds.size.y;
+        float spriteX =spriteRenderer.sprite.bounds.size.x;
+
+        float scaleX = screenX / spriteX;
+        float scaleY = screenY / spriteY;
+
+        float uniformScale = Mathf.Min(scaleX, scaleY);
+
+        transform.localScale = new Vector3(uniformScale, uniformScale, 1f);
+    }
+}
---
+++ Assets/Script/DialogSystem.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DialogSystem : MonoBehaviour
+{
+    public TextAsset jsonfile;
+    public List<Sprite> sprites;
+
+    [System.Serializable]
+    public class Dialogrows
+    {
+        public int dialog_ID; // 0
+        public string dialog_type; // #
+        public string name; // a
+        public string dialog; // a1
+        public string dialog_position; // down
+        public string image_name; // a1
+        public string image_position; // left
+        public int dialog_next; // 1
+        public string command; // 
+    }
+
+    public Dictionary<string,Sprite> Dic_Name_Image = new Dictionary<string,Sprite>();
+
+    public TMP_Text dialogtext;
+
+    public List<Dialogrows> dialogrow=new List<Dialogrows>();
+
+    public void ReadDialog(string str)
+    {
+        jsonfile = Resources.Load<TextAsset>(str);
+        dialogrow= JsonConvert.DeserializeObject<List<Dialogrows>>(jsonfile.text);
+    }
+
+    public void ShowText(string text,string text_position)
+    {
+
+    }
+
+    public void ShowImage(string image_name,string image_position)
+    {
+
+    }
+
+    public void ShowBackground(string image_background)
+    {
+
+    }
+
+    public void ShowDialog()
+    {
+
+    }
+
+    public void Awake()
+    {
+        ReadDialog("storyline");
+
+    }
+
+}
